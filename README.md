8932a9f6a351415256d86266a7e34e5d334dd0ed
palaashGupta1 <65058970+palaashGupta1@users.noreply.github.com>
2020-09-05 20:33:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pinco-game
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,22 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+//var ground1
+
+
+function setup() {
+  createCanvas(480,800);
+
+//ground1=new Ground(240,790,10,480);
+
+
+}
+
+function draw() {
+  background(0);  
+  
+  
+  //ground1.display();
+  drawSprites();
+}
